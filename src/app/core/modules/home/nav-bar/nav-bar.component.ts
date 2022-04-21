4de9fe9bd562f70b6c3077f46e38aebfc612a8ea
dali-trabelsi <mohamedmuhamed010@gmail.com>
2022-04-21 03:06:22 +0100

    updated global navbar

--- src/app/core/modules/home/nav-bar/nav-bar.component.ts
+++ src/app/core/modules/home/nav-bar/nav-bar.component.ts
@@ -1,6 +1,9 @@
 import { AuthService } from './../../../services/auth/auth.service';
 import { Router } from '@angular/router';
 import { Component, Inject, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/core/services/user/users.service';
+import { UserModel } from 'src/app/core/models/user.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'home-page-nav-bar',
@@ -9,15 +12,30 @@ import { Component, Inject, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   constructor(
-    @Inject(Router) private router: Router,
-    public auth: AuthService
+    @Inject(Router) public router: Router,
+    public auth: AuthService,
+    private userService: UsersService
   ) {}
   isLoggedIn: boolean = false;
+  showDrawer: boolean = true;
+
+  user!: UserModel;
 
   ngOnInit(): void {
     const url = this.router.url;
-    if (['/course/create', '/courses/list'].includes(url)) {
+    if (!['/', '/login', '/signup', '/contact'].includes(url)) {
       this.isLoggedIn = true;
     }
+    this.userService.getUserInfo().subscribe({
+      next: (res: UserModel) => {
+        this.user = res;
+        this.user.avatar = this.user.avatar
+          ? environment.apiUrl + '/media/' + this.user.avatar
+          : environment.userAvatarImgPlaceholder;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
