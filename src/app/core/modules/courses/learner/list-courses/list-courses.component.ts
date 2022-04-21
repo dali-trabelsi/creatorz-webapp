e92c4fa06ea6683c3adbb8f2b257c664d1d31716
dali-trabelsi <mohamedmuhamed010@gmail.com>
2022-04-21 03:05:56 +0100

    added course listing and course details components

--- src/app/core/modules/courses/learner/list-courses/list-courses.component.ts
+++ src/app/core/modules/courses/learner/list-courses/list-courses.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { CourseModel } from './../../../../models/course.model';
+import { CoursesService } from 'src/app/core/services/courses/course.service';
+import { Component, OnInit, Inject } from '@angular/core';
+import jwtDecode from 'jwt-decode';
 
+const token = jwtDecode(localStorage.getItem('accessToken') || '') as {
+  role: string;
+};
 @Component({
   selector: 'app-list-courses',
   templateUrl: './list-courses.component.html',
-  styleUrls: ['./list-courses.component.scss']
+  styleUrls: ['./list-courses.component.scss'],
 })
 export class ListCoursesComponent implements OnInit {
+  courses: CourseModel[] = [];
+  userRole = token.role;
 
-  constructor() { }
+  constructor(
+    private courseService: CoursesService,
+    @Inject(Router) router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.courseService.getAllCourses().subscribe((res) => {
+      res.forEach((course: CourseModel) => {
+        course.thumbnail = environment.apiUrl + '/media/' + course.thumbnail;
+      });
+      this.courses = res;
+    });
   }
-
 }
